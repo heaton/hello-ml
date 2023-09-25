b67d7168407b65316a669147959c57ff1f8d946b
Heaton <heaton.cai@gmail.com>
2023-09-25 22:03:46 +1000

    a simple linear regression demo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Learn Machine Learning
+
+### Environment setup
+
+```
+brew install python3
+pip3 install numpy
+```
+
+### Run the code
+
+`python3 linear-regression.py`
---
+++ linear-regression.py
@@ -0,0 +1,74 @@
+import math
+import numpy as np
+
+def compute_cost(x, y, w, b):
+    m = x.shape[0]
+    cost = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = cost + (f_wb - y[i])**2
+    total_cost = 1 / (2 * m) * cost
+
+    return total_cost
+
+
+def compute_gradient(x, y, w, b):
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function):
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+
+    for i in range(num_iters):
+        dj_dw, dj_db = gradient_function(x, y, w, b)
+
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+        if i < 100000:      # prevent resource exhaustion
+            J_history.append(cost_function(x, y, w, b))
+            p_history.append([w, b])
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
+                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                  f"w: {w: 0.3e}, b:{b: 0.5e}")
+
+    return w, b, J_history, p_history
+
+
+# trainning data
+features = np.array([1.0, 2.0])
+targets = np.array([300.0, 500.0])
+
+# initialize parameters
+w_init = 0
+b_init = 0
+
+# some gradient descent settings
+iterations = 10000
+tmp_alpha = 1.0e-2
+
+# run gradient descent
+w_final, b_final, J_hist, p_hist = gradient_descent(features, targets, w_init, b_init, tmp_alpha,
+                                                    iterations, compute_cost, compute_gradient)
+
+print(f"(w,b) found by gradient descent: ({w_final:8.4f},{b_final:8.4f})")
